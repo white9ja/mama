e9a759cca2676362d1550d2aede691579615bfc5
harrison <harrison.o.ebube@gmail.com>
2021-08-02 15:12:46 +0100

    commit for deploy to heroku

--- nne/__init__.py
+++ nne/__init__.py
@@ -29,7 +29,7 @@ app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
 app.config['MAIL_USERNAME'] = 'cellulantharry@gmail.com'
-app.config['MAIL_PASSWORD'] = 'advancegrace2016'
+app.config['MAIL_PASSWORD'] = 'advancegrace201'
 mail = Mail(app)
 
 
