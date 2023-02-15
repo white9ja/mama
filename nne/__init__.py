1b31ecfda559aa1871692602f2f42f76e2d9ba02
harrison <harrison.o.ebube@gmail.com>
2023-02-15 14:24:45 +0100

    fixed the issuues with bcrypt

--- nne/__init__.py
+++ nne/__init__.py
@@ -17,7 +17,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database/mama2.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 #Create database connection object
-# os.environ.get('EMAIL_USER')
+# os.environ.get('EMAIL_USER')   
 # os.environ.get('EMAIL_PASS')
 
 db = SQLAlchemy(app)
@@ -31,6 +31,13 @@ app.config['MAIL_USE_TLS'] = True
 app.config['MAIL_USERNAME'] = 'cellulantharry@gmail.com'
 app.config['MAIL_PASSWORD'] = 'advancegrace201'
 mail = Mail(app)
+
+
+from nne.models import Users, User, Workers, Guarrantor, Payment
+
+@login_manager.user_loader
+def load_user(user_id):
+	return User.query.get(int(user_id))
 
 
 from nne import routes 
